d7b472dd827994788f9c4780d70a269dfb9a0d8f
Prabhav Khandelwal <prabhav@Prabhavs-MacBook-Pro.local>
2022-11-03 23:18:30 -0400

    NEW CHANGE

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,9 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to my app!" />
+        <h1>HELLO THIS IS A BIG CHANCE</h1>
         <p className="description">
+          hello hello
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
